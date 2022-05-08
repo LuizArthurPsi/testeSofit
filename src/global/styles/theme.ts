9f229b26c984122b1f4c113e21501e66765de4cf
Luiz Arthur <luizarthurvm@gmail.com>
2022-05-08 11:08:50 -0300

    Add Global color themes

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    primary: '#c7d84e',
+
+    success: '#34af23',
+    attention: '#e83f5b',
+
+    shape: '#ffffff',
+    title: '#2e3030',
+    text: '#666666',
+  },
+};
