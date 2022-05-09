547d4dad2934a5c89fcbbda6f4cb3312e54fb385
Luiz Arthur <luizarthurvm@gmail.com>
2022-05-08 23:53:25 -0300

    Update Register
    Added form and input's validation

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 
 import { Container, Form, Header, Title, Fields } from './styles';
 
 export function Register() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [additionalInfo, setAdditionalInfo] = useState('');
+
+  function handleRegister() {
+    if (!name || !price) {
+      Alert.alert('Insira valores válidos');
+    } else {
+      const data = {
+        Name: name,
+        Price: price,
+        Info: additionalInfo,
+      };
+      console.log(data);
+    }
+  }
+
   return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-      <Form>
-        <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Valor" />
-          <Input placeholder="Informações adicionais" />
-        </Fields>
-        <Button title="Enviar" />
-      </Form>
-    </Container>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+
+        <Form>
+          <Fields>
+            <Input
+              placeholder="Nome"
+              onChangeText={setName}
+              autoCapitalize="sentences"
+              autoCorrect={false}
+            />
+
+            <Input
+              placeholder="Valor"
+              onChangeText={setPrice}
+              keyboardType="numeric"
+            />
+
+            <Input
+              placeholder="Informações adicionais"
+              onChangeText={setAdditionalInfo}
+              autoCapitalize="sentences"
+              autoCorrect={false}
+            />
+          </Fields>
+
+          <Button
+            title="Enviar"
+            onPress={() => {
+              handleRegister();
+            }}
+          />
+        </Form>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
