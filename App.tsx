0e27265d110085db42f39f9dff8d692ea4fce0ae
Luiz Arthur <luizarthurvm@gmail.com>
2022-05-08 21:00:50 -0300

    Add Header do Register

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
-import Dashboard from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
